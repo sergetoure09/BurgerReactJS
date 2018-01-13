55372273ce4ef09c4314572aadfea31e3fcaf941
sergetoure09 <sergetoure09@gmail.com>
2018-01-13 00:13:37 +0000

    merging really ok now

--- src/App.js
+++ src/App.js
@@ -3,41 +3,14 @@ import classes from './App.css';
 import BurgerBuilder from './containers/burgerbuilder'
 import Aux from './components/aux'
 
-
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      name:"Best Whoooper!",
-      price:"$19,99",
-      description:"The favorite burger of business man!"
-    }
-
-  }
- 
-  render() {
-   
-    return (
-    <div className="App">
-    <BurgerInfo name={this.state.name} price={this.state.price} description={this.state.description}/>
   
-      <Bread> 
-              <Ingredient.Patty/>  
-              <Ingredient.Patty/>
-              <Ingredient.Cheese />
-              <Ingredient.Tomato />
-              <Ingredient.Cheese/>
-              <Ingredient.Cheese/>
-              <Ingredient.Cheese/>
-              <Ingredient.Onion />
-              <Ingredient.Meatball />
-              <Ingredient.Meatball />
-              <Ingredient.Meatball />
-              <Ingredient.Pickle/>
-              <Ingredient.Patty/>
-      </Bread>
-      <Plate/>
-      </div>
+  render() {
+     return (
+    <Aux classes={classes.App}>
+    <BurgerBuilder />
+    </Aux>
+    
     );
   }
 }
